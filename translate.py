a2e69b7fabe2ab27df67e4b7cf94da9c1104f305
Sanghyeon_Jung <sanghyeon_jung@tmax.co.kr>
2022-02-17 11:23:46 +0900

    [ADD] description

--- translate.py
+++ translate.py
@@ -91,14 +91,18 @@ def main(args) :
         """
 
         for i in range(max_len-1):
-            memory = memory.to(DEVICE)
+            memory = memory.to(DEVICE) # encoder를 통과한 input 시퀀스
             tgt_mask = (generate_square_subsequent_mask(ys.size(0))
                         .type(torch.bool)).to(DEVICE)
-            out = model.decode(ys, memory, tgt_mask)
+            
+            # 현재까지 생성된 decoder의 output과 encoder를 통과한 input 시퀀스, 그리고 타겟마스크를 decoder에 입력함 
+            out = model.decode(ys, memory, tgt_mask) 
             out = out.transpose(0, 1)
-            prob = model.generator(out[:, -1])
-            _, next_word = torch.max(prob, dim=1)
-            next_word = next_word.item()
+
+            # 디코더 결과를 linear layer에 통과시켜 타겟 vocab size 만큼 차원을 맞추어 줌
+            prob = model.generator(out[:, -1]) # decoder output의 마지막 토큰 한 글자만 활용함
+            _, next_word = torch.max(prob, dim=1) # the result tuple of two output tensors (max, max_indices)
+            next_word = next_word.item() # 가장 probable한 단어 index를 next_word로 입력함
 
             ys = torch.cat([ys,
                             torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=0)
@@ -115,6 +119,8 @@ def main(args) :
         src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool)
         tgt_tokens = greedy_decode(
             model,  src, src_mask, max_len=num_tokens + 5, start_symbol=BOS_IDX).flatten()
+
+        # 어휘집에서 lookup_tokens 함수로 인덱스를 단어로 치환하여 문자열을 생성한 후 리턴함 
         return " ".join(vocab_transform[TGT_LANGUAGE].lookup_tokens(list(tgt_tokens.cpu().numpy()))).replace("<bos>", "").replace("<eos>", "")
 
 
